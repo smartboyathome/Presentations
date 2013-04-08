1bc945a147b55f12368e5d032b54ca64fa93daa6
smartboyathome <smartboyathome@gmail.com>
2013-04-07 23:48:06 -0700

    Added CraftyJS and started on the Haxxit demo.

--- static/js/Haxxit-Proposal.js
+++ static/js/Haxxit-Proposal.js
@@ -1,3 +1,45 @@
+/*
+ * Crafty code
+ */
+
+var Game = {
+    start: function(stage_elem) {
+        Crafty.init($(stage_elem).width(), $(stage_elem).height(), stage_elem);
+
+        Crafty.scene('main', function() {
+            Crafty.background('#07093D');
+            Crafty.e('2D, DOM, Text')
+                .attr({w: 800, h: 20, x: 0, y: 160})
+                .text("Haxxit")
+                .css({
+                    'text-align': 'center',
+                    'font-family': 'Digital-7',
+                    'font-size': '4em'
+                });
+            Crafty.e('2D, DOM, Text, Mouse')
+                .attr({w: 200, h: 40, x: 300, y: 440})
+                .text('START')
+                .css({
+                    'text-align': 'center',
+                    'font-family': 'Mode Seven',
+                    'font-size': '1em',
+                    'background': '#107FC9'
+                }).bind('Click', function(e) {
+                    alert('click!!!');
+                });
+        });
+
+        Crafty.scene("main");
+    },
+    stop: function() {
+        Crafty.stop(true);
+    }
+};
+
+/*
+ * Reveal code
+ */
+
 Reveal.addEventListener( 'title', function() {
     $('html h1.title .normal').animate({
         'opacity': 1
@@ -13,3 +55,15 @@ Reveal.addEventListener( 'title', function() {
         }, 400, 'easeInBounce');
     });
 }, false );
+
+Reveal.addEventListener('slidechanged', function (event) {
+    var elem = $('section.present #haxxit');
+    if(elem.length === 0)
+    {
+        Crafty.stop(true); // stops and clears the state
+    }
+    else
+    {
+        Game.start(elem[0]);
+    }
+});
